42bd0d727d4a09272af7874cda33e5ad1d222f89
Jasyd Caballero <jasydavid45@gmail.com>
2021-02-12 18:51:38 -0500

    added masonry card styles to gif search results

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,31 @@
-import { Link } from "wouter";
+import { useState } from "react";
+import { Link, useLocation } from "wouter";
 
 const Home = () => {
+  const [location, setLocation] = useLocation();
   const tags = ["ferrari", "subaru", "anime", "japon", "korea", "otter"];
+  const [keyword, setKeyword] = useState("");
 
+  const handleSubmit = (e) => setLocation(`/search/${keyword}`);
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  console.log(keyword);
   return (
     <>
-      {tags.map((tag) => (
-        <Link to={`/search/${tag}`}>Gif de {tag}</Link>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="keyword"
+          placeholder="Search Gifs here"
+          onChange={handleChange}
+        />
+      </form>
+      {tags.map((tag, i) => (
+        <Link key={i} to={`/search/${tag}`}>
+          Gif de {tag}
+        </Link>
       ))}
     </>
   );
